407ea4acbc97a9b141dd4669ce17ef521f55958c
ronick <glflakcm@gmail.com>
2021-03-18 21:35:58 +0900

    데이터베이스 정보 업데이트

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace DotnetCoreServer.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value num1", "value num2" };
         }
 
         // GET api/values/5
