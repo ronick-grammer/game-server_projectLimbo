97f0874d34027f961410acc2b6a8c3e050202483
ronick-grammer <glflakcm@gmail.com>
2021-03-18 22:25:41 +0900

    Add existing file

---
+++ Models/Data/AppInfo.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppInformation
+{
+    public static class Info
+    {
+        public const string rest_api_key = "904d6ba81c3d04c5918546a8c170e385";
+        public const string base_url = "https://kauth.kakao.com";
+        public const string redirect_url = "https://server-projectlimbo.azurewebsites.net/Login/KakaoLogin";
+    }
+}
